809eaa33d91375984fc57db08f9871098a012800
agniesia <s15038@pjwstk.edu.pl>
2019-10-12 19:10:39 +0200

    List of pokemons in Player class

--- my-app/src/main/java/com/mycompany/app/Player.java
+++ my-app/src/main/java/com/mycompany/app/Player.java
@@ -1,14 +1,20 @@
 package com.mycompany.app;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Player {
 
     protected String name;
+    protected List<Pokemon> pokemons;
 
     public Player(){
         this.name = "noName";
+        this.pokemons = new ArrayList<>();
     }
     public Player(String name){
         this.name = name;
+        this.pokemons = new ArrayList<>();
     }
 
 }
--- my-app/src/test/java/com/mycompany/app/PlayerTest.java
+++ my-app/src/test/java/com/mycompany/app/PlayerTest.java
@@ -15,7 +15,19 @@ public class PlayerTest {
 
     @Test
     public void creationPlayerWithNameTest(){
-        Assert.assertNotNull(new Player("Janek"));
+        Assert.assertNotNull(new Player("John"));
     }
+
+    @Test
+    public void pokemonListExists(){
+        Player player = new Player();
+        Assert.assertNotNull(player.pokemons);
+    }
+    @Test
+    public void pokemonListExistsInPlayerWithName(){
+        Player player = new Player("John");
+        Assert.assertNotNull(player.pokemons);
+    }
+
 
 }
